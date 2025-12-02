964dd7ddc0c64eb7382957a117162bae2eb003a0
Adam Carpenter <apcarp@users.noreply.github.com>
2025-12-02 12:31:09 -0500

    Add __version__ to jlab_archiver_client package (#12)

--- src/jlab_archiver_client/__init__.py
+++ src/jlab_archiver_client/__init__.py
@@ -1,3 +1,6 @@
+"""This package provides a Python interface to the JLab Archiver's myquery service."""
+import importlib.metadata
+
 from .query import (IntervalQuery as IntervalQuery, MySamplerQuery as MySamplerQuery, ChannelQuery as ChannelQuery,
                    PointQuery as PointQuery, MyStatsQuery as MyStatsQuery)
 from .mysampler import MySampler as MySampler
@@ -5,3 +8,5 @@ from .interval import Interval as Interval
 from .point import Point as Point
 from .channel import Channel as Channel
 from .mystats import MyStats as MyStats
+
+__version__ = importlib.metadata.version("jlab_archiver_client")
